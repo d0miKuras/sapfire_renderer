c58e6e0aed9920f6178142326b08196ea84d4143
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-04-24 21:46:20 +0200

    Added cgmath crate

--- src/lib.rs
+++ src/lib.rs
@@ -46,12 +46,19 @@ const INDICES_DATA: [u32; 6] = [0, 1, 2, 2, 3, 0];
 const VERTEX_SHADER: &str = "
 #version 450
 
+layout(binding = 0) uniform UniformBufferObject {
+    mat4 model;
+    mat4 view;
+    mat4 proj;
+} ubo;
+
 layout(location = 0) in vec2 inPosition;
 layout(location = 1) in vec3 inColor;
+
 layout(location = 0) out vec3 fragColor;
 
 void main() {
-    gl_Position = vec4(inPosition, 0.0, 1.0);
+    gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 0.0, 1.0);
     fragColor = inColor;
 }";
 
